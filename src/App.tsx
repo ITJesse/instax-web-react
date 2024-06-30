98729856331e65ed0d5efd10c46d4b339d6ebbbc
Jesse Zhu <jesse@itjesse.com>
2024-06-30 23:13:55 +0900

    Refactor UI and cleanup redundant CSS styles
    - Streamline the main UI by simplifying the structure and removing unused CSS classes and stylings.
    - Opt out of specific style customizations for elements like buttons and headings to enhance maintenance and uniformity.
    - Refactor global styles to remove color scheme and responsiveness related adjustments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import './App.css'
 
-import viteLogo from '/vite.svg'
 import { useRef } from 'react'
 
-import reactLogo from './assets/react.svg'
 import { AssetsLoader } from './components/AssetsLoader'
 import { InstaxPrinter } from './lib/instax'
 
@@ -19,24 +17,7 @@ function App() {
 
   return (
     <AssetsLoader>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleConnect}>connect</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleConnect}>connect</button>
     </AssetsLoader>
   )
 }
